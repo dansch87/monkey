323d1cb8611eec2f18b02190d25370d124871dc6
Daniel Schulz <daniel.schulz.lg@gmail.com>
2023-09-19 20:36:53 +0200

    Create skipWhitespace function and enhance NextToken function by that helper function to eat up whitespace between literals

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -52,6 +52,9 @@ func (l *Lexer) NextToken() token.Token {
 
 	var tok token.Token
 
+	// eat whitespace
+	l.skipWhitespace()
+
 	// Create a Token object
 	switch l.ch {
 	case '=':
@@ -89,4 +92,10 @@ func (l *Lexer) NextToken() token.Token {
 	l.readChar()
 
 	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
