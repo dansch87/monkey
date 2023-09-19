006394089be065e1b2b55b5891ece460b7d49673
Daniel Schulz <daniel.schulz.lg@gmail.com>
2023-09-19 17:16:06 +0200

    Add test routine for basic tokens to support development of lexer

---
+++ lexer/lexer_test.go
@@ -0,0 +1,44 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/dansch87/monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	// Init a new lexer
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		// test for type
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		// test for literals
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+
+	}
+
+}
