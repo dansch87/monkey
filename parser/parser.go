fdf32bdf53e8d5b65ad9c8a03fd6bb3c5f40982d
Daniel Schulz <daniel.schulz.lg@gmail.com>
2023-09-24 12:40:54 +0200

    Add basic program structure for the parser. Contains the Parser struct, constructor func New() and helper function nextToken(). The ParseProgram() func doesn't do anything at that point

---
+++ parser/parser.go
@@ -0,0 +1,34 @@
+package parser
+
+import (
+	"github.com/dansch87/monkey/ast"
+	"github.com/dansch87/monkey/lexer"
+	"github.com/dansch87/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	// helper method to advance bot curToken and peekToken
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
