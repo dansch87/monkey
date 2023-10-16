dd12679bf5bf5ddb6f2b232a0aaafed7c77ec2e5
Daniel Schulz <daniel.schulz.lg@gmail.com>
2023-10-16 19:44:13 +0200

    Add parse method for integer literal

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/dansch87/monkey/ast"
 	"github.com/dansch87/monkey/lexer"
@@ -181,4 +182,19 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(ep.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
