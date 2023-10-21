12f5eee607bfcae54b9b9313afb94087e08f461d
Daniel Schulz <daniel.schulz.lg@gmail.com>
2023-10-21 09:47:48 +0200

    Add documentation for the token.go file

--- token/token.go
+++ token/token.go
@@ -1,3 +1,5 @@
+// Package defines the basic data structure and helper functions that are required for the translation 
+// of monkey source code into Tokens that is conducted during the lexical analysis (lexer).
 package token
 
 type TokenType string
@@ -7,6 +9,7 @@ type Token struct {
 	Literal string
 }
 
+// Definition and assignment of TokenType to constants
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
@@ -22,10 +25,8 @@ const (
 	BANG     = "!"
 	ASTERISK = "*"
 	SLASH    = "/"
-
 	LT = "<"
 	GT = ">"
-
 	EQ     = "=="
 	NOT_EQ = "!="
 
@@ -48,6 +49,8 @@ const (
 	RETURN   = "RETURN"
 )
 
+// Initialize hash table that contains a mapping between identifier strings and there 
+// corresponding TokenType (Token).
 var keywords = map[string]TokenType{
 	"fn":     FUNCTION,
 	"let":    LET,
@@ -58,11 +61,12 @@ var keywords = map[string]TokenType{
 	"return": RETURN,
 }
 
+// LookupIdent receives an identifier string (e.g. "fn", "let", etc.) and lookup and returns 
+// it's appropriate TokenType from a hash table. If there occures no match at all, the default 
+// TokenType IDENT is returned.
 func LookupIdent(ident string) TokenType {
-	// If ident exists in keyword hash table return its TokenType
-	// Otherwise return IDENT TokenType
-	if tok, ok := keywords[ident]; ok {
-		return tok
+	if token, ok := keywords[ident]; ok {
+		return token
 	}
 	return IDENT
 }
